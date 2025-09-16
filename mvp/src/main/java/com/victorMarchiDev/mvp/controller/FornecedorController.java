fdf4c24504542f1327965fb1ded3eb0b63a3da69
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-09-16 20:46:54 -0300

    commit

---
+++ mvp/src/main/java/com/victorMarchiDev/mvp/controller/FornecedorController.java
@@ -0,0 +1,35 @@
+package com.victorMarchiDev.mvp.controller;
+
+import com.victorMarchiDev.mvp.model.FornecedorModel;
+import com.victorMarchiDev.mvp.service.FornecedorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/fornecedor")
+public class FornecedorController {
+
+    private final FornecedorService service;
+    public FornecedorController(FornecedorService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<FornecedorModel> criarFornecedor(@RequestBody FornecedorModel fornecedor){
+            service.criarFornecedor(fornecedor);
+            return ResponseEntity.ok().body(fornecedor);
+    }
+
+    @GetMapping
+    public List<FornecedorModel> listarFornecedores(){
+        return service.listarFornecedores();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarFornecedor(@PathVariable Long id){
+        service.deletarFornecedor(id);
+    }
+
+}
