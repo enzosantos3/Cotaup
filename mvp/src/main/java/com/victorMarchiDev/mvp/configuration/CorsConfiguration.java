61134b970b22b2ae9a359f1f1c773ecb0f92543b
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-09-17 20:34:56 -0300

    correções

--- mvp/src/main/java/com/victorMarchiDev/mvp/configuration/CorsConfiguration.java
+++ mvp/src/main/java/com/victorMarchiDev/mvp/configuration/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
