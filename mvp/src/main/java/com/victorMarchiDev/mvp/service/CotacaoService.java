a60750e9138763ae0971da7f767f670fcfc6b391
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-11-07 22:33:12 -0300

    ajustando frontend para receber os produtos na resposta da cotação

--- mvp/src/main/java/com/victorMarchiDev/mvp/service/CotacaoService.java
+++ mvp/src/main/java/com/victorMarchiDev/mvp/service/CotacaoService.java
@@ -2,10 +2,13 @@ package com.victorMarchiDev.mvp.service;
 
 
 import com.victorMarchiDev.mvp.model.CotacaoModel;
+import com.victorMarchiDev.mvp.model.ProdutoModel;
 import com.victorMarchiDev.mvp.repository.CotacaoRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -19,12 +22,18 @@ public class CotacaoService {
     }
 
     public CotacaoModel criarCotacao(CotacaoModel cotacaoModel){
-        LocalDate hoje = LocalDate.now();
+        LocalDate hoje = LocalDate.now(ZoneId.of("America/Sao_Paulo"));
         LocalDate seteDiasDepois = hoje.plusDays(7);
         cotacaoModel.setDataInicio(hoje);
         cotacaoModel.setDataFim(seteDiasDepois);
 
         return repo.save(cotacaoModel);
+    }
+
+    public List<ProdutoModel> listarProdutosPorCotacao(Long id) {
+        return repo.findById(id)
+                .map(CotacaoModel::getProdutosCotacao)
+                .orElse(Collections.emptyList());
     }
 
     public List<CotacaoModel> listarCotacoes() {
