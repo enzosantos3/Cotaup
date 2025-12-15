5e0d611a1ff289357d98ff41e0ad9ceba2e1179d
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-12-14 22:26:37 -0300

    v.4.4.10 - Eliminando dados incorretos do repositorio
    Removendo os  arquivos e pastas  que foram  enviados para o repositório de forma incorreta

--- front-end/.next/dev/server/app/api/cotacoes/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/cotacoes/route.js")
-R.c("server/chunks/node_modules_next_2b53daa8._.js")
-R.c("server/chunks/[root-of-the-server]__32edb2d2._.js")
-R.c("server/chunks/_next-internal_server_app_api_cotacoes_route_actions_da2b01e9.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/cotacoes/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/cotacoes/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- front-end/.next/dev/server/chunks/_next-internal_server_app_api_cotacoes_route_actions_da2b01e9.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/cotacoes/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_cotacoes_route_actions_da2b01e9.js.map
